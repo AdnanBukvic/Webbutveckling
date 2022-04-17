77fd62c39f747afa77cb347ec471c95138165233
Adnan Bukvic EEDAT20 <89768167+AdnanBukvic@users.noreply.github.com>
2022-04-18 00:04:46 +0200

    Snabb polish

--- Slutuppgift/toggle-menu.js
+++ Slutuppgift/toggle-menu.js
@@ -1,8 +1,11 @@
 function visaMeny() {
     var header = document.getElementById("header");
+
+
     if (header.style.visibility === "hidden") {
         header.style.visibility = "visible";
     } else {
         header.style.visibility = "hidden";
     }
+    
 }
